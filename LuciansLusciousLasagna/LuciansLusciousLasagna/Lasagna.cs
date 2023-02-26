5209231f589e172818729eeb799b5cf1f41eb1e9
Mohsin Khan <mohsink9899@gmail.com>
2023-02-26 23:32:08 +0530

    Lucian's Luscious Lasagna

---
+++ LuciansLusciousLasagna/LuciansLusciousLasagna/Lasagna.cs
@@ -0,0 +1,29 @@
+﻿namespace LuciansLusciousLasagna
+{
+    class Lasagna
+    {
+        // TODO: define the 'ExpectedMinutesInOven()' method
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+        // TODO: define the 'RemainingMinutesInOven()' method
+        public int RemainingMinutesInOven(int MinutesBeenInOven)
+        {
+            return (40 - MinutesBeenInOven);
+        }
+
+        // TODO: define the 'PreparationTimeInMinutes()' method
+        public int PreparationTimeInMinutes(int NumberOfLayers)
+        {
+            return (NumberOfLayers * 2);
+        }
+
+        // TODO: define the 'ElapsedTimeInMinutes()' method
+        public int ElapsedTimeInMinutes(int NumberOfLayers, int MinutesBeenInOven)
+        {
+            int TimeTakenToPrepareLayers = NumberOfLayers * 2;
+            return (TimeTakenToPrepareLayers + MinutesBeenInOven);
+        }
+    }
+}
---
+++ LuciansLusciousLasagna/LuciansLusciousLasagna/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuciansLusciousLasagna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lasagna lasagna = new Lasagna();
+
+            Console.WriteLine(lasagna.ExpectedMinutesInOven());
+            
+            Console.WriteLine(lasagna.RemainingMinutesInOven(25));
+
+            Console.WriteLine(lasagna.PreparationTimeInMinutes(1));
+            Console.WriteLine(lasagna.PreparationTimeInMinutes(4));
+
+            Console.WriteLine(lasagna.ElapsedTimeInMinutes(1, 30));
+            Console.WriteLine(lasagna.ElapsedTimeInMinutes(4, 8));
+            Console.ReadKey();
+        }
+    }
+}
