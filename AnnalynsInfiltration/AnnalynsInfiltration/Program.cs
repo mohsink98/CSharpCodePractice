d8326e3ff3864514448fc3f3e7cb7e8d709aee9f
Mohsin Khan <mohsink9899@gmail.com>
2023-02-28 01:14:41 +0530

    Annalyn's Infiltration

---
+++ AnnalynsInfiltration/AnnalynsInfiltration/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnalynsInfiltration
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(QuestLogic.CanFastAttack(true));
+            Console.WriteLine(QuestLogic.CanFastAttack(false));
+
+
+            Console.WriteLine(QuestLogic.CanSpy(false, false, false));
+            Console.WriteLine(QuestLogic.CanSpy(true, false, false));
+            Console.WriteLine(QuestLogic.CanSpy(false, true, false));
+            Console.WriteLine(QuestLogic.CanSpy(false, false, true));
+            Console.WriteLine(QuestLogic.CanSpy(false, true, true));
+            Console.WriteLine(QuestLogic.CanSpy(true, false, true));
+            Console.WriteLine(QuestLogic.CanSpy(true, true, false));
+            Console.WriteLine(QuestLogic.CanSpy(true, true, true));
+
+
+            Console.WriteLine(QuestLogic.CanSignalPrisoner(false, false));
+            Console.WriteLine(QuestLogic.CanSignalPrisoner(false, true));
+            Console.WriteLine(QuestLogic.CanSignalPrisoner(true, false));
+            Console.WriteLine(QuestLogic.CanSignalPrisoner(true, true));
+
+
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, false, false, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, false, false, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, false, true, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, false, true, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, true, false, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, true, false, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, true, true, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(false, true, true, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, false, false, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, false, false, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, false, true, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, false, true, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, true, false, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, true, false, true));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, true, true, false));
+            Console.WriteLine(QuestLogic.CanFreePrisoner(true, true, true, true));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AnnalynsInfiltration/AnnalynsInfiltration/QuestLogic.cs
@@ -0,0 +1,64 @@
+﻿    static class QuestLogic
+    {
+        public static bool CanFastAttack(bool knightIsAwake)
+        {
+            if (knightIsAwake == false)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+        {
+            if (knightIsAwake == true || archerIsAwake == true || prisonerIsAwake == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+        {
+            if (archerIsAwake == false && prisonerIsAwake == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+        {
+            if (petDogIsPresent == true)
+            {
+                if (archerIsAwake == false && knightIsAwake == true && prisonerIsAwake == true)
+                {
+                    return true;
+                }
+
+                if (archerIsAwake == false && knightIsAwake == false && prisonerIsAwake == true)
+                {
+                    return true;
+                }
+
+                if (archerIsAwake == false && knightIsAwake == true && prisonerIsAwake == false)
+                {
+                    return true;
+                }
+
+                if (archerIsAwake == false && knightIsAwake == false && prisonerIsAwake == false)
+                {
+                    return true;
+                }
+            }
+
+            if (petDogIsPresent == false)
+            {
+                if (archerIsAwake == false && knightIsAwake == false && prisonerIsAwake == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
